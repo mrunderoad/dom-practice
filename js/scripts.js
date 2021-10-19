af37bf5c27038c9514288e7c09a2841bf7c5bc6b
Isaac Overstreet <isaacoverstreet3@gmail.com>
2021-10-19 15:30:14 -0700

    Add more js to remove children
    co-authored-by: Ella Tanttu <ellajtanttu@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,15 @@ $(document).ready(function() {
   $("button#hello").click(function() {
     $("ul#user").prepend("<li>Hello!</li>");
     $("ul#webpage").prepend("<li>Why hello there!</li>");
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
+    // $('li').click(function() {
+    //   alert('hi');
+    // });
   });
 
   $("button#goodbye").click(function() {
@@ -13,4 +22,13 @@ $(document).ready(function() {
     $("ul#user").prepend("<li>Stop copying me!</li>");
     $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
   });
+
+  //$("ul#user").children("li").first().click(function() {
+   // alert('hi');
+  //});
+
+  //$("ul#webpage").children("li").first().click(function() {
+  //  alert('hi');
+ // });
+
 });
